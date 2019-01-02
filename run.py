148d95cd05334b5159e4aceb4be88f8c7dcb088f
Ezra MacDonald <macdonaldezra@gmail.com>
2019-01-02 10:22:04 -0800

    Minor updates to routes and bootstrap page

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-	return render_template('index.html')
+# both GET and POST are used in order to server the index
+# page and handle form submissions.
+@app.route("/", methods=['GET', 'POST'])
+def index():
+	errors = []
+	results = {}
+	if request.method == "POST":
+		try:
+			diary = request.form['diary']
+			
+		except:
+			errors.append(
+				"Unable to read text."
+				)
+		# get the text that the user entered
+	return render_template('index.html', errors=errors, results=results)
